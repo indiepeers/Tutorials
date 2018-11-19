975685509d293af1bbf4cbfd6f5ee59f003eea87
indiepeers <45165613+indiepeers@users.noreply.github.com>
2018-11-19 17:43:46 +0800

    Added simple playerprefs script

---
+++ SimplePrefs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePrefs : MonoBehaviour {
+
+	public int CurrentNumber;
+	public int PreviousNumber;
+
+	void Start () {
+
+		if (PlayerPrefs.GetInt("storedNumber") != null) PreviousNumber = PlayerPrefs.GetInt("storedNumber");
+
+		CurrentNumber = Random.Range (0, 999);
+
+		PlayerPrefs.SetInt ("storedNumber", CurrentNumber);
+	}
+}
